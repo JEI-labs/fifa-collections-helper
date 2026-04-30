02bd257243a76e749777ab3dfc920980bdc8ae96
Iago Carmona <iagoortegacarmona@gmail.com>
2026-04-29 21:00:32 -0300

    adjusting ocr and layout frame

--- components/CameraScanner.tsx
+++ components/CameraScanner.tsx
@@ -397,7 +397,7 @@ export default function CameraScanner({ onScan }: CameraScannerProps) {
       {debugImage && (
         <img
           src={debugImage}
-          className="absolute top-20 left-4 w-32 border rounded-md border-red-500 z-50"
+          className="absolute top-[50%] left-4 w-32 border rounded-md border-red-500 z-50"
         />
       )}
     </div>
